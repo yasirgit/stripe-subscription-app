c666d4ad09932faf0e91ccb277547e963ec248df
Yasir Arefin <sobujarefin@gmail.com>
2024-12-12 15:00:53 +0100

    fix test failure in github

--- test/controllers/webhooks_controller_test.rb
+++ test/controllers/webhooks_controller_test.rb
@@ -1,12 +1,14 @@
 require "test_helper"
+require "openssl"
 
 class WebhooksControllerTest < ActionDispatch::IntegrationTest
   test "should get create" do
     payload = { id: 'evt_test_webhook', type: 'customer.subscription.created' }.to_json
+    timestamp = Time.now.to_i
+    secret = Rails.configuration.stripe[:webhook_secret]
+    signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), secret, "#{timestamp}.#{payload}")
     headers = {
-      'HTTP_STRIPE_SIGNATURE' => Stripe::Webhook::Signature.compute_signature_header(
-        Time.now.to_i, payload, Rails.configuration.stripe[:webhook_secret]
-      )
+      'HTTP_STRIPE_SIGNATURE' => "t=#{timestamp},v1=#{signature}"
     }
 
     post '/webhooks', params: payload, headers: headers
