f601945b1ae064e0c3f1aba2a372a0f14f005769
Yasir Arefin <sobujarefin@gmail.com>
2024-12-12 14:59:13 +0100

    fix test failure in github

--- test/controllers/webhooks_controller_test.rb
+++ test/controllers/webhooks_controller_test.rb
@@ -2,7 +2,14 @@ require "test_helper"
 
 class WebhooksControllerTest < ActionDispatch::IntegrationTest
   test "should get create" do
-    post '/webhooks', params: { body: '{}' }
+    payload = { id: 'evt_test_webhook', type: 'customer.subscription.created' }.to_json
+    headers = {
+      'HTTP_STRIPE_SIGNATURE' => Stripe::Webhook::Signature.compute_signature_header(
+        Time.now.to_i, payload, Rails.configuration.stripe[:webhook_secret]
+      )
+    }
+
+    post '/webhooks', params: payload, headers: headers
     assert_response :success
   end
 end
