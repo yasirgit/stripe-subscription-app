d1b470d4e2b865fe20e0aa04c55dc11562fc4c76
Yasir Arefin <sobujarefin@gmail.com>
2024-12-12 15:03:00 +0100

    fix test failure in github

--- test/controllers/webhooks_controller_test.rb
+++ test/controllers/webhooks_controller_test.rb
@@ -2,6 +2,11 @@ require "test_helper"
 require "openssl"
 
 class WebhooksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @webhook_secret = 'whsec_test_secret'
+    Rails.configuration.stripe = { webhook_secret: @webhook_secret }
+  end
+
   test "should get create" do
     payload = { id: 'evt_test_webhook', type: 'customer.subscription.created' }.to_json
     timestamp = Time.now.to_i
